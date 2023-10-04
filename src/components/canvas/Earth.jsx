94d422ff730d75facb93e48f879a03d448ad3024
athenamw <wallisathena@gmail.com>
2023-10-03 19:42:47 -0700

    create stars

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -33,10 +33,11 @@ const EarthCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         />
-      </Suspense>
-      <Earth />
+      
+        <Earth />
 
-      <Preload all />
+        <Preload all />
+      </Suspense>
     </Canvas>
   );
 };
